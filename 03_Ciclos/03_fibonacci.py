8c2bf23f7652fdeb0767f6145fa8a592e28f6a31
erpinowacky <90427634+erpinowacky@users.noreply.github.com>
2023-02-18 14:01:33 -0500

    Update 03_fibonacci.py

--- 03_Ciclos/03_fibonacci.py
+++ 03_Ciclos/03_fibonacci.py
@@ -1,12 +1,18 @@
 '''Escriba un programa para mostrar n términos de número natural y su suma (Fibonacci).'''
 
-fibonacci = 10 #int(input('Digite un número entero: '))
-num1 = 1
+fibonacci = 5 #int(input('Digite un número entero: '))
+num1 = 0
 num2 = 0
 resultado = 0
 
-for i in range(fibonacci+1):
-    resultado = num1 + num2
-    num2 = num1
-    num1 = resultado 
-    print(resultado)        
+for i in range(fibonacci):
+    if i == 0:
+        pass
+    elif i == 1:
+        resultado = 1
+        num1 = resultado
+    else:
+        resultado = num1 + num2
+        num2 = num1
+        num1 = resultado 
+    print(resultado)       
